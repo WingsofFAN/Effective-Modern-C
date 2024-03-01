2ca83f82373abd72d473f355504649d3eb746b2b
yangfan5 <yangfan5@sensetime.com>
2024-03-01 19:55:30 +0800

    理解 std::move 和 std::forward 区分万能引用和右值引用 针对右值引用实施 std::move, 针对万能引用实施 std::forward

--- src/charpt5 References/25.cpp
+++ src/charpt5 References/25.cpp
@@ -38,7 +38,6 @@ private:
     std::shared_ptr<someData> data;
 };
 
-
 class B
 {
 public:
@@ -64,9 +63,60 @@ private:
     std::shared_ptr<someData> data;
 };
 
+class C
+{
+public:
+    C(C&& rhs)                    //rhs 是个右值引用
+    :name(std::move(rhs.name)),   //移动
+     data(std::move(rhs.data))    //移动
+    {}
 
+    C(std::string name_,someData data_)
+    :name(name_)
+    {data = std::make_shared<someData>(data_);}
+    
+    void setName(const std::string& newName)   
+    {name= newName;}                           //从常量左值取得赋值
 
+    void setName(std::string&& newName)     
+    {name= std::move(newName); }               //从右值取得赋值
 
+    void printName()
+    { std::cout << name << std::endl; }
+
+private:
+    std::string name;
+    std::shared_ptr<someData> data;
+};
+
+/*
+c++ 自身有返回值优化,return value optimization RVO
+
+Widget makeWidget()
+{
+    Widget w;
+    return w;
+}
+
+手动优化
+Widget makeWidget()
+{
+    Widget w; ...
+    return std::move(w);  // w会作为右值处理,原来是复制省略却没有实施
+}
+
+与一以下代码效果相同:
+
+Widget makeWidget()
+{
+    Widget w; ...
+    return w;
+}
+
+事实上:针对函数中按值返回的局部对象实施std::move的操作,不能给编译器帮上忙
+(如果不执行复制省略,就必须将局部对象作为右值处理,效果一样),却可以帮倒忙(可能会排除掉RVO的实施机会)
+
+*/
 
 
 int main()
@@ -95,6 +145,18 @@ int main()
     c.printName();                       //
 
     std::cout << Name2 << std::endl;     //局部变量会消失
+
+    std::string Name3 = "Name3";         //
+    C d("name3", adata);                 //
+    d.setName(Name3);                    //
+    d.printName();
+    d.setName("name4");                  //
+    d.printName();
+
+
+
+
+
 
 
     return 0;
